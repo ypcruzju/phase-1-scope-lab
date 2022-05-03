123c84908688f69d343db235c1589ac8081a35a0
yaxpaccruzjuarez <ypjuarez11@gmail.com>
2022-05-03 16:28:12 -0400

    commit tree b192079ad94e918017bc42e49474712cc139561b

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName ='bob'
+
+function upperCaseCustomerName(){
+   return customerName = 'BOB'
+}
+
+
+function setBestCustomer(){
+
+    return bestCustomer = 'not bob'
+}
+
+
+function overwriteBestCustomer(){
+    return bestCustomer = 'maybe bob'
+
+}
+const leastFavoriteCustomer = 'Yax'
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'Joe'
+}
